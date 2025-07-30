392ec2ea526f9cdd8c9d2add1513cc3716347385
Mariana_k <m.koval000@gmail.com>
2025-07-30 12:44:33 +0300

    add loader

--- src/components/Loader/loader.tsx
+++ src/components/Loader/loader.tsx
@@ -1,10 +1,10 @@
-import { HashLoader } from "react-spinners";
+import { RiseLoader } from "react-spinners";
 import css from "./Loader.module.css";
 
 export default function Loader() {
   return (
     <div className={css.loaderWrapper}>
-      <HashLoader color="#088169ff" loading={true} size={50} />
+      <RiseLoader color="#d541d9" loading={true} size={30} />
     </div>
   );
 }
